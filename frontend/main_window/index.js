07d9dfac717d75fa501c13edbc16e004e7b022ea
Cemre Cander <cemrecander@gmail.com>
2022-08-31 18:17:25 +0300

    Button actions added

--- frontend/main_window/index.js
+++ frontend/main_window/index.js
@@ -1,4 +1,12 @@
+const { ipcRenderer } = require("electron");
+
 function howToUse() {
     let myModal = new bootstrap.Modal(document.getElementById('myModal'), {});
     myModal.show();
 }
+
+let btnStart = document.getElementById("btnStart");
+
+btnStart.addEventListener("click", () => {
+    ipcRenderer.send("openWebcamWindow");
+});
