5479df21c4f1aa487292c6c4f422d4690f4cb6ab
Cemre Cander <cemrecander@gmail.com>
2022-08-31 18:19:50 +0300

    webcam window and tray added

--- frontend/main.js
+++ frontend/main.js
@@ -1,30 +1,85 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, ipcMain, Menu, Tray, BrowserWindow } = require('electron')
+
+let win;
+let webcamWindow;
+let tray = null;
 
 function createWindow() {
-    const win = new BrowserWindow({
+    win = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
+            nodeIntegration: true,
+            contextIsolation: false,
         }
     })
+    win.loadFile(__dirname + "./main_window/index.html")
+    win.on('minimize', function (event) {
+        event.preventDefault();
+        win.hide();
 
-    win.loadFile('index.html')
+    });
+
+    win.on('close', function (event) {
+        if (!app.isQuiting) {
+            event.preventDefault();
+            win.hide();
+        }
+        return false;
+    });
 }
 
 app.whenReady().then(() => {
     createWindow()
+
+    tray = new Tray('images/img8.jpg')
+    const contextMenu = Menu.buildFromTemplate([
+        {
+            label: 'Quit',
+            type: 'normal',
+            click: function () {
+                app.exit();
+            }
+        }
+    ])
+    tray.setToolTip('Sign Language Translator')
+    tray.setContextMenu(contextMenu)
+
+    tray.on('double-click', function (e) {
+        win.show();
+    });
 
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
             createWindow()
         }
     })
+
+    function createWebcamWindow() {
+        webcamWindow = new BrowserWindow({
+            width: 800,
+            height: 700,
+            modal: true,
+            show: false,
+            parent: win,
+            webPreferences: {
+                nodeIntegration: true,
+                contextIsolation: false,
+                enableRemoteModule: true,
+            }
+        });
+        webcamWindow.loadFile(__dirname + "./webcam_window/index.html");
+
+        webcamWindow.once("ready-to-show", () => {
+            webcamWindow.show();
+        });
+    }
+    ipcMain.on("openWebcamWindow", () => {
+        createWebcamWindow();
+    })
 })
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
+    app.quit()
 })
+
